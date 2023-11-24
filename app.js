b8a7c83cb856a56457e8c5f80eafa40f6c381c74
Saeed Torkaman <saeedtorkaman15@gmail.com>
2023-11-24 13:08:41 +0330

    create read and write stream

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 const fs = require('fs');
 
-// fs.writeFile('./docs/blog1.txt', 'write text', (err) =>{ // write file
-//     if (err)
-//             console.log(err);
-// })
+const readString = fs.createReadStream('./docs/blog.txt')
 
-// fs.appendFile('./docs/blog1.txt', '\nappend text', (err)=>{ // append file
-//     if (err)
-//         console.log(err);
-//     else{
-//         fs.readFile('./docs/blog1.txt',  (err , data) => { // read file
-//             if (err)
-//                 console.log(err);
-//             else
-//                 console.log(data.toString());
-//         })
-//     }
-// })
-
-// fs.mkdir('./assets', (err)=>{ // create directory
-//     (err)
-//         console.log(err);
-//     console.log('folder created');
-// })
-// fs.rmdir('./assets' , (err)=>{ // remove directory
-//     if(err)
-//         console.log(err);
-//     console.log('remove directory');
-// })
-
-// fs.unlink('./docs/blog1.txt', (err)=>{ // remove file
-//     if(err)
-//         console.log(err);
-//     console.log('file removing');
-// })
+readString.on('data', (buffer)=>{
+    console.log(buffer);
+})
